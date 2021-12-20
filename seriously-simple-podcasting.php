4ea618c48a1713c27138534c7e676262b1cbd4f9
Martin Kirchgessner <martinkirch@users.noreply.github.com>
2021-12-20 12:27:14 +0100

    version 1.11.3

--- seriously-simple-podcasting.php
+++ seriously-simple-podcasting.php
@@ -1,11 +1,11 @@
 <?php
 /*
  * Plugin Name: Seriously Simple Podcasting - Radio Campus edition
- * Version: 1.11.2
+ * Version: 1.11.3
  * Description: Variante de "Seriously Simple Podcasting" pour Radio Campus Grenoble
  * Author: Hugh Lashbrooke, Martin Kirchgessner
  * Requires at least: 4.2
- * Tested up to: 4.3.1
+ * Tested up to: 5.8.2
  *
  * Text Domain: seriously-simple-podcasting
  * Domain Path: /lang/
@@ -21,8 +21,8 @@ require_once( 'includes/class-ssp-admin.php' );
 require_once( 'includes/class-ssp-frontend.php' );
 
 global $ssp_admin, $ss_podcasting;
-$ssp_admin = new SSP_Admin( __FILE__, '1.11.2' );
-$ss_podcasting = new SSP_Frontend( __FILE__, '1.11.2' );
+$ssp_admin = new SSP_Admin( __FILE__, '1.11.3' );
+$ss_podcasting = new SSP_Frontend( __FILE__, '1.11.3' );
 
 if ( is_admin() ) {
 	global $ssp_settings;
